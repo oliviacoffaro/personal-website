4afc985bff59c0325e1b28a8334f10d98366af7b
Olivia Coffaro <ofcoffaro@gmail.com>
2017-02-13 14:14:37 -0500

    params, show and index methods

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,18 @@
 class PostsController < ApplicationController
+
+  def index
+    @posts = Post.all
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  private
+    def params
+      params.require(:post).permit(:title, :content)
+    end
+
+
+
 end
--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,2 +1,15 @@
 class ProjectsController < ApplicationController
+
+  def index
+    @projects = Project.all
+  end
+
+  def show
+    @project = Project.find(params[:id])
+  end
+
+  private
+    def params
+      params.require(:project).permit(:title, :content)
+    end
 end
