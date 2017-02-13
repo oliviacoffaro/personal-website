4ad092c6229b0edf325071332902d39ffa0f11c4
Olivia Coffaro <ofcoffaro@gmail.com>
2017-02-13 16:24:23 -0500

    adding bootstrap cdn, container to entire page

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,10 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-posts = Post.create([{ title: "My First Hackathona", Content: "This past weekend I had a right of passage as a developer....attending my first Hackathon.  I've been involved with the Girl Develop It organization for about a year and a half now.  Their vision is 'to create a network of empowered women who feel confident in their abilities to code and build beautiful web and mobile applications'."}])
+posts = Post.create([{ title: "My First Hackathona", Content: "This past weekend I had a right of passage as a developer....attending my first Hackathon.  I've been involved with the Girl Develop It organization for about a year and a half now.  Their vision is 'to create a network of empowered women who feel confident in their abilities to code and build beautiful web and mobile applications'.  Etsy hosted and they have a beautiful space, we were lucky to have their resources.  I had a few major take aways from the event
+
+  1. Man, it felt good to be coding.  There is just something special about 'hacking' away at a project.  I definitely entered that coding Nirvana space that you are super focused and efficient.
+
+  2. Speak up! I didn't enter the Hackathon with a set team or set plan.  While this turned out totally ok, there is the learning curve of never working together.  The best thing for everyone on the team is being clear about your own abilities and what you would like to contribute.  It took the morning to get an idea together, but by the afternoon we had clear roles and were working harmoniously.
+
+  3. Community.  There is a big difference from my first tech meetup to now.  I've grown from just recognizing faces, to actually knowing people, and now even have inside jokes!  It's empowering to build a community with other female developers to support and grow."}])
