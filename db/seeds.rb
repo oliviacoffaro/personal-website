e84ddd89880ef169b02a226cc5f105c6f001e6ba
Olivia Coffaro <ofcoffaro@gmail.com>
2017-02-13 15:44:21 -0500

    post seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+posts = Post.create([{ title: "My First Hackathona", Content: "This past weekend I had a right of passage as a developer....attending my first Hackathon.  I've been involved with the Girl Develop It organization for about a year and a half now.  Their vision is 'to create a network of empowered women who feel confident in their abilities to code and build beautiful web and mobile applications'."}])
